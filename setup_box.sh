b885a207fd6adfb2c056dde55485f1be345cbeb5
Cody Garrett <4968312+qwertycody@users.noreply.github.com>
2019-02-05 16:01:48 -0500

    Update setup_box.sh

--- setup_box.sh
+++ setup_box.sh
@@ -1,3 +1,6 @@
+#Copy and paste Line 2 on to the linux box to download the entire script and setup the box (Debian 9 64 Bit or higher)
+#$SUDO_COMMAND sh <(curl -sL https://raw.githubusercontent.com/voyagersclan/scripts/master/setup_box.sh)
+
 SUDO_COMMAND=""
 OPAM_COMMAND="$SUDO_COMMAND opam"
 INSTALL_COMMAND="$SUDO_COMMAND apt-get install -y"
