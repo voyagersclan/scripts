bbfec7c634c2de7c14e8696c7ba991d1e84f0ca8
Cody Garrett <4968312+qwertycody@users.noreply.github.com>
2019-02-05 16:39:15 -0500

    Changed app to DriveSync built on Ruby

--- setup_box.sh
+++ setup_box.sh
@@ -2,36 +2,24 @@
 #$SUDO_COMMAND sh <(curl -sL https://raw.githubusercontent.com/voyagersclan/scripts/master/setup_box.sh)
 
 SUDO_COMMAND=""
-OPAM_COMMAND="$SUDO_COMMAND opam"
 INSTALL_COMMAND="$SUDO_COMMAND apt-get install -y"
 
 #Run As Root
 $SUDO_COMMAND dpkg --configure -a
 
 $INSTALL_COMMAND curl
-
-#Optional for OPAM Main Program
-$INSTALL_COMMAND make
-$INSTALL_COMMAND m4
-$INSTALL_COMMAND cc
-$INSTALL_COMMAND gcc
-
-#Optional for OPAM Features
-$INSTALL_COMMAND rsync
 $INSTALL_COMMAND git
-$INSTALL_COMMAND hg
-$INSTALL_COMMAND darcs
 
-#Required for OPAM
-$INSTALL_COMMAND patch
-$INSTALL_COMMAND unzip
-$INSTALL_COMMAND bubblewrap
+$INSTALL_COMMAND ruby-full
+$SUDO_COMMAND gem install bundler
 
-#Install OPAM
-$SUDO_COMMAND sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+$SUDO_COMMAND git clone https://github.com/MStadlmeier/drivesync.git
+$SUDO_COMMAND cd drivesync
+$SUDO_COMMAND bundle install
+$SUDO_COMMAND ruby drivesync.rb
 
-#Initialize OPAM
-$OPAM_COMMAND init
-$OPAM_COMMAND update
-$OPAM_COMMAND install depext
-$OPAM_COMMAND install google-drive-ocamlfuse
+echo "ruby /root/drivesync/drivesync.rb" > /etc/cron.hourly/updateDrive.sh
+
+chmod 777 /etc/cron.hourly/updateDrive.sh
+
+#Setup will be complete at this point and the files will be syncing to /root/Documents/drive
