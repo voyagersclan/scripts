fbfa7214d6f45c036a15646d80cbebd9262248aa
Cody Garrett <cody@garrett.ms>
2019-04-09 18:24:02 -0400

    bedrock

--- hyperv_setup.ps1
+++ hyperv_setup.ps1
@@ -3,3 +3,5 @@ New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias “vEth
 New-NetNAT -Name “NATNetwork” -InternalIPInterfaceAddressPrefix 192.168.0.0/24
 Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 22 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 22 -NatName NATNetwork
 Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 25565 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 25565 -NatName NATNetwork
+Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 19132 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 19132 -NatName NATNetwork
+Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 19133 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 19133 -NatName NATNetwork
