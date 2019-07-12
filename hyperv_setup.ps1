0fdc9294f730aeeff5a0b4a2d7a75ddfa31cea71
Cody Garrett <cody@garrett.ms>
2019-07-12 13:25:53 -0400

    more robust.

--- hyperv_setup.ps1
+++ hyperv_setup.ps1
@@ -1,7 +1,57 @@
-New-VMSwitch -SwitchName “NATSwitch” -SwitchType Internal
-New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceAlias “vEthernet (NATSwitch)”
-New-NetNAT -Name “NATNetwork” -InternalIPInterfaceAddressPrefix 192.168.0.0/24
-Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 22 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 22 -NatName NATNetwork
-Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 25565 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 25565 -NatName NATNetwork
-Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 19132 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 19132 -NatName NATNetwork
-Add-NetNatStaticMapping -ExternalIPAddress "0.0.0.0/0" -ExternalPort 19133 -Protocol TCP -InternalIPAddress "192.168.0.93" -InternalPort 19133 -NatName NATNetwork
+$VM_SWITCH_NAME="NATSwitch"
+$VM_SWITCH_TYPE="Internal"
+
+$NET_IP_ADDRESS="192.168.0.1"
+$NET_PREFIX_LENGTH="24"
+$NET_INTERFACE_ALIAS="vEthernet (NATSwitch)"
+
+$NET_NAT_NAME="NATNetwork"
+$NET_INTERNAL_IP_INTERFACE_ADDRESS_PREFIX="192.168.0.0/24"
+
+$NET_NAT_STATIC_MAPPING_EXTERNAL_IP_ADDRESS="0.0.0.0/0"
+$NET_NAT_STATIC_MAPPING_INTERNAL_IP_ADDRESS="192.168.0.93"
+
+
+function main()
+{
+    echo "Removing/Adding Switch ($VM_SWITCH_NAME)..."
+    Remove-VMSwitch -Force -name $VM_SWITCH_NAME -confirm:$false
+    New-VMSwitch -SwitchName $VM_SWITCH_NAME -SwitchType $VM_SWITCH_TYPE
+
+    echo "Resetting/Setting IP Range Rules ($NET_INTERFACE_ALIAS)..."
+    Remove-NetIPAddress -InterfaceAlias $NET_INTERFACE_ALIAS -confirm:$false
+    New-NetIPAddress -IPAddress $NET_IP_ADDRESS -PrefixLength $NET_PREFIX_LENGTH -InterfaceAlias $NET_INTERFACE_ALIAS
+
+    echo "Removing/Adding NAT Network ($NET_NAT_NAME)..."
+    Remove-NetNat -Name $NET_NAT_NAME -confirm:$false
+    New-NetNAT -Name $NET_NAT_NAME -InternalIPInterfaceAddressPrefix $NET_INTERNAL_IP_INTERFACE_ADDRESS_PREFIX
+
+    Setup_PortForwarding -Port 22
+    Setup_PortForwarding -Port 25565
+    Setup_PortForwarding -Port 19132
+    Setup_PortForwarding -Port 19133
+}
+
+function Setup_NetNatStaticMapping()
+{
+    param($ExternalIPAddress, $ExternalPort, $Protocol, $InternalIPAddress, $InternalPort, $NatName)
+    
+    $NetNatStaticMappingList = Get-NetNatStaticMapping 
+
+    foreach($NetNatStaticMapping in $NetNatStaticMappingList)
+    {
+        Remove-NetNatStaticMapping -StaticMappingID $NetNatStaticMapping.StaticMappingID -confirm:$false
+    }
+
+    Add-NetNatStaticMapping -ExternalIPAddress $ExternalIPAddress -ExternalPort $ExternalPort -Protocol $Protocol -InternalIPAddress $InternalIPAddress -InternalPort $InternalPort -NatName $NatName
+}
+
+function Setup_PortForwarding()
+{
+    param($Port)
+
+    echo "Removing/Adding Port Forwarding ($Port)..."
+    Setup_NetNatStaticMapping -ExternalIPAddress $NET_NAT_STATIC_MAPPING_EXTERNAL_IP_ADDRESS -ExternalPort $Port -Protocol TCP -InternalIPAddress $NET_NAT_STATIC_MAPPING_INTERNAL_IP_ADDRESS -InternalPort $Port -NatName $NET_NAT_NAME
+}
+
+main
